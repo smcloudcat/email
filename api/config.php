6791f1eeffecdeedaa2d0995a7e89ce8cbbcf248
云猫 <3522934828@qq.com>
2024-04-21 11:37:31 +0000

    update api/config.php.
    Signed-off-by: 云猫 <3522934828@qq.com>

--- api/config.php
+++ api/config.php
@@ -1,13 +1,5 @@
 <?php return array (
   'mailboxes' => 
   array (
-    'xiccsend@163.com' => 
-    array (
-      'smtp' => 'smtp.163.com',
-      'port' => '465',
-      'secu' => 'ssl',
-      'username' => 'xiccsend@163.com',
-      'password' => 'JTCUWNSGDRBYDFKZ',
-    ),
   ),
 );
